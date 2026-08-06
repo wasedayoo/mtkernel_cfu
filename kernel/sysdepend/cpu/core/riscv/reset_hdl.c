31e04965f0d023d1527dcbc480f3b1abef122d75
futatsugiyuuma <wasedayoo@gmail.com>
2026-08-06 18:24:09 +0900

    finish test 5

--- kernel/sysdepend/cpu/core/riscv/reset_hdl.c
+++ kernel/sysdepend/cpu/core/riscv/reset_hdl.c
@@ -7,6 +7,7 @@ volatile uint32_t test_status = 0xc001d00d;
 volatile const uint32_t rodata_value = 0xa5a55a5a;
 volatile uint32_t initialized_data = 0x13579bdf;
 volatile uint32_t zero_initialized_data;
+volatile uint32_t csr_actual;
 
 extern uint32_t __bss_start[];
 extern uint32_t __bss_end[];
@@ -33,6 +34,86 @@ static uint32_t stack_test(uint32_t high, uint32_t low)
     return local[3];
 }
 
+static inline void write_mstatus(uint32_t value)
+{
+    __asm__ volatile ("csrw mstatus, %0" : : "r" (value));
+}
+
+static inline uint32_t read_mstatus(void)
+{
+    uint32_t value;
+
+    __asm__ volatile ("csrr %0, mstatus" : "=r" (value));
+    return value;
+}
+
+static inline void write_mie(uint32_t value)
+{
+    __asm__ volatile ("csrw mie, %0" : : "r" (value));
+}
+
+static inline uint32_t read_mie(void)
+{
+    uint32_t value;
+
+    __asm__ volatile ("csrr %0, mie" : "=r" (value));
+    return value;
+}
+
+static uint32_t csr_test(void)
+{
+    uint32_t actual;
+
+    write_mstatus(0x00000008);
+    actual = read_mstatus();
+    if (actual != 0x00000008) {
+        csr_actual = actual;
+        return 0xdead0004;
+    }
+
+    write_mstatus(0x00000000);
+    actual = read_mstatus();
+    if (actual != 0x00000000) {
+        csr_actual = actual;
+        return 0xdead0005;
+    }
+
+    write_mie(0x00000080);
+    actual = read_mie();
+    if (actual != 0x00000080) {
+        csr_actual = actual;
+        return 0xdead0006;
+    }
+
+    write_mie(0x00000000);
+    actual = read_mie();
+    if (actual != 0x00000000) {
+        csr_actual = actual;
+        return 0xdead0007;
+    }
+
+    return 0;
+}
+
+static uint32_t startup_csr_test(void)
+{
+    uint32_t actual;
+
+    actual = read_mstatus();
+    if (actual != 0x00000000) {
+        csr_actual = actual;
+        return 0xdead0008;
+    }
+
+    actual = read_mie();
+    if (actual != 0x00000000) {
+        csr_actual = actual;
+        return 0xdead0009;
+    }
+
+    return 0;
+}
+
 void Reset_Handler(void)
 {
     uint32_t result;
@@ -41,7 +122,14 @@ void Reset_Handler(void)
     zero_initialized_data = 0xffffffff;
     clear_bss();
 
-    if (rodata_value != 0xa5a55a5a) {
+    result = startup_csr_test();
+    if (result == 0) {
+        result = csr_test();
+    }
+
+    if (result != 0) {
+        /* csr_test() has already set a diagnostic result. */
+    } else if (rodata_value != 0xa5a55a5a) {
         result = 0xdead0001;
     } else if (initialized_data != 0x13579bdf) {
         result = 0xdead0002;
